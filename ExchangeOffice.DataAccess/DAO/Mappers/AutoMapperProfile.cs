b17bdee4cbb8349a9b12e3ee7f3dab8dd06db8c4
DESKTOP-LIRSJS6\Klova <nikolayklova404@gmail.com>
2024-05-19 21:45:36 +0300

    fix some bugs in reservations + small refactor

--- ExchangeOffice.DataAccess/DAO/Mappers/AutoMapperProfile.cs
+++ ExchangeOffice.DataAccess/DAO/Mappers/AutoMapperProfile.cs
@@ -40,5 +40,14 @@ namespace ExchangeOffice.DataAccess.DAO.Mappers {
 				.ForMember(dest => dest.ModifiedOn, opt => opt.MapFrom(src => DateTime.UtcNow))
 				.ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
 		}
+		private void CreateReservationMapper() {
+			CreateMap<Reservation, Reservation>()
+				.ForMember(dest => dest.Contact, opt => opt.MapFrom(src => src.Contact))
+				.ForMember(dest => dest.Rate, opt => opt.MapFrom(src => src.Rate))
+				.ForMember(dest => dest.Id, opt => opt.Ignore())
+				.ForMember(dest => dest.CreatedOn, opt => opt.Ignore())
+				.ForMember(dest => dest.ModifiedOn, opt => opt.MapFrom(src => DateTime.UtcNow))
+				.ForMember(dest => dest.IsActive, opt => opt.MapFrom(src => true));
+		}
 	}
 }
--- ExchangeOffice.DataAccess/Repositories/ReservationRepository.cs
+++ ExchangeOffice.DataAccess/Repositories/ReservationRepository.cs
@@ -1,4 +1,5 @@
-﻿using ExchangeOffice.Common.Exceptions;
+﻿using AutoMapper;
+using ExchangeOffice.Common.Exceptions;
 using ExchangeOffice.DataAccess.DAO;
 using ExchangeOffice.DataAccess.Repositories.Abstractions;
 using ExchangeOffice.DataAccess.Repositories.Interfaces;
@@ -7,21 +8,19 @@ using Microsoft.EntityFrameworkCore;
 namespace ExchangeOffice.DataAccess.Repositories {
 	public class ReservationRepository : BaseRepository, IReservationRepository {
 		private readonly DataAccessContext _context;
-		private readonly IRateRepository _rateRepo;
-		private readonly IContactRepository _contactRepo;
-		public ReservationRepository(DataAccessContext context, IRateRepository rateRepo, IContactRepository contactRepo) {
+		private readonly IMapper _mapper;
+		public ReservationRepository(DataAccessContext context, IMapper mapper) {
 			_context = context;
-			_rateRepo = rateRepo;
-			_contactRepo = contactRepo;
+			_mapper = mapper;
 		}
 
 		public async Task<IEnumerable<Reservation>> GetReservationsAsync() {
 			return await Task.FromResult(_context.Reservations
 				.Include(x => x.Contact)
 				.Include(x => x.Rate)
-					.ThenInclude(r => r.TargetCurrency) // Включаем связанный объект TargetCurrency
-					.Include(x => x.Rate)
-					.ThenInclude(r => r.BaseCurrency) // Включаем связанный объект BaseCurrency
+				.ThenInclude(r => r.TargetCurrency)
+				.Include(x => x.Rate)
+				.ThenInclude(r => r.BaseCurrency)
 				.Where(x=>x.IsActive)
 				.AsNoTracking());
 
@@ -31,21 +30,18 @@ namespace ExchangeOffice.DataAccess.Repositories {
 			var entity = await _context.Reservations
 				.Include(x => x.Contact)
 				.Include(x => x.Rate)
-					.ThenInclude(r => r.TargetCurrency) // Включаем связанный объект TargetCurrency
-					.Include(x => x.Rate)
-					.ThenInclude(r => r.BaseCurrency) // Включаем связанный объект BaseCurrency
-				.Where(x => x.Id == id)
+				.ThenInclude(r => r.TargetCurrency)
+				.Include(x => x.Rate)
+				.ThenInclude(r => r.BaseCurrency)
+				.Where(x => x.Id == id && x.IsActive == true)
 				.AsNoTracking()
 				.FirstOrDefaultAsync();
-			if (entity == null || entity.IsActive == false) {
+			if (entity == null) {
 				throw new RecordNotFoundException(404, "DataAccess", "Reservation with such id not found");
 			}
 			return entity;
 		}
 		public async Task<Reservation> AddReservationAsync(Reservation entity) {
-			entity.Rate = await _rateRepo.GetRateByIdAsync(entity.RateId);
-			entity.Contact = await _contactRepo.GetContactAsync(entity.ContactId);
-			
 			SetDefaultValues(entity);
 			await _context.Reservations.AddAsync(entity);
 			await _context.SaveChangesAsync();
@@ -53,26 +49,24 @@ namespace ExchangeOffice.DataAccess.Repositories {
 
 		}
 		public async Task<Reservation> UpdateReservationAsync(Reservation entity) {
-			var oldEntity = await _context.Reservations.FindAsync(entity.Id);
-			if (oldEntity == null || entity.IsActive == false) {
+			var oldEntity = await _context.Reservations
+				.Where(x => x.Id == entity.Id && x.IsActive == true)
+				.FirstOrDefaultAsync();
+			if (oldEntity == null) {
 				throw new RecordNotFoundException(404, "DataLayer", "Reservation with such id not found");
 			}
-			entity.Id = oldEntity.Id;
-			entity.ModifiedOn = DateTime.UtcNow;
-			entity.Rate = await _rateRepo.GetRateByIdAsync(entity.RateId);
-			entity.Contact = await _contactRepo.GetContactAsync(entity.ContactId);
-			_context.Reservations.Remove(oldEntity);
-			await _context.Reservations.AddAsync(entity);
+			_mapper.Map(entity, oldEntity);
 			await _context.SaveChangesAsync();
 			return entity;
 		}
 		public async Task<Reservation> DeleteReservationAsync(Guid id) {
-			var entity = await _context.Reservations.FindAsync(id);
-			if (entity == null || entity.IsActive == false) {
+			var entity = await _context.Reservations
+				.Where(x=>x.Id==id && x.IsActive == true)
+				.FirstOrDefaultAsync();
+			if (entity == null) {
 				throw new RecordNotFoundException(404, "DataAccess", "Reservation with such id not found");
 			}
-			entity.ModifiedOn = DateTime.UtcNow;
-			entity.IsActive = false;
+			SetDeleteDefaultValues(entity);
 			await _context.SaveChangesAsync();
 			return entity;
 		}
