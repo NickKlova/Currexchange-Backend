0610d01512c8e110491d96df6cf82dcccb72779b
DESKTOP-LIRSJS6\Klova <nikolayklova404@gmail.com>
2024-05-19 21:53:36 +0300

    user bufix

--- ExchangeOffice.DataAccess/Repositories/UserRepository.cs
+++ ExchangeOffice.DataAccess/Repositories/UserRepository.cs
@@ -12,31 +12,24 @@ namespace ExchangeOffice.DataAccess.Repositories {
 		}
 
 		public async Task<User> GetUserAsync(string login) {
-			var entity = await _context.Users.Where(x=>x.Login == login && x.IsActive == true).FirstOrDefaultAsync();
+			var entity = await _context.Users
+				.Where(x=>x.Login == login && x.IsActive == true)
+				.FirstOrDefaultAsync();
+			if (entity == null) {
+				throw new RecordNotFoundException(404, "DataAccess", "User with such login not found");
+			}
 			return entity;
 		}
 		public async Task<User> AddUserAsync(User entity) {
-			if (entity.RoleId != null) {
-				var roleEntity = await _context.UserRoles.FindAsync(entity.RoleId);
-				if (roleEntity != null) {
-					entity.Role = roleEntity;
-				}
-			}
-			
-			if (entity.ContactId != null) {
-				var contactEntity = await _context.Contacts.FindAsync(entity.ContactId);
-				if (contactEntity != null) {
-					entity.Contact = contactEntity;
-				}
-			}
-
 			SetDefaultValues(entity);
 			await _context.Users.AddAsync(entity);
 			await _context.SaveChangesAsync();
 			return entity;
 		}
 		public async Task<User> ChangeUserRoleAsync(string login, Guid roleId) {
-			var entity = await _context.Users.Where(x => x.Login == login && x.IsActive == true).FirstOrDefaultAsync();
+			var entity = await _context.Users
+				.Where(x => x.Login == login && x.IsActive == true)
+				.FirstOrDefaultAsync();
 			if (entity == null) {
 				throw new RecordNotFoundException(404, "DataAccess", "User with such id not found");
 			}
